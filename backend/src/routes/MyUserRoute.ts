dba152e890834699b802deb5cda25db9087052fd
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-05-24 14:48:20 +0300

    Auth token

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 //if we get a request - post - this handler is gonna get called - handle business logic
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck , MyUserController.createCurrentUser);
 
 export default router;
