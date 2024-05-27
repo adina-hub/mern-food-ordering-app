6bb1e28944fdd2523ab291ee5a082a0a4e0a96e8
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-05-27 15:30:45 +0300

    Update user profile api

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -20,4 +20,27 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { createCurrentUser };
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
+
+export default { createCurrentUser, updateCurrentUser };
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 //if we get a request - post - this handler is gonna get called - handle business logic
-router.post("/", jwtCheck , MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
