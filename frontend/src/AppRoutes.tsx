db2395198065014ee455acacea16b4cc6a89deb2
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-05-27 16:12:49 +0300

    User Profile Form

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,17 +1,32 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout><HomePage /></Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout showHero>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AppRoutes;
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -4,19 +4,18 @@ import Hero from "@/components/Hero";
 
 type Props = {
   children: React.ReactNode;
-}
+  showHero?: boolean;
+};
 
-function Layout({ children } : Props) {
+function Layout({ children, showHero = false }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
-      <div className="container mx-auto flex-1 py-10">
-        {children}
-      </div>
+      {showHero && <Hero />}
+      <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
-  )
+  );
 }
 
 export default Layout;
