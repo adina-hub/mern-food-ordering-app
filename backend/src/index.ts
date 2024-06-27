03334cf6b043a5afc2c8bdbd482dc6e7f317543c
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-06-27 16:01:46 +0300

    Created Restaurant API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-import { v2 as cloudinary } from "cloudinary"
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to database");
@@ -12,8 +13,8 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -24,6 +25,8 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the API");
