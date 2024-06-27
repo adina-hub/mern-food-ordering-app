990975057f82d66135f1b3c01edaeb500444d3a5
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-06-27 15:12:35 +0300

    Cloudinary setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to database");
 });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 const app = express();
-
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/health", async (req: Request, res: Response) => {
