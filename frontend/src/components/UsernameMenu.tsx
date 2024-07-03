e35e58cea57565884937c82d690f3cc6bc07b71c
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-07-03 14:20:49 +0300

    Created restaurant form

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -1,5 +1,10 @@
 import { CircleUserRound } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { Separator } from "./ui/separator";
@@ -11,21 +16,26 @@ const UsernameMenu = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center px-3 font-bold hover: text-orange-500 gap-2">
-        <CircleUserRound className="text-orange-500"/>
+        <CircleUserRound className="text-orange-500" />
         {user?.email}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
-          <Link 
-            to="/user-profile" 
+          <Link
+            to="/manage-restaurant"
             className="font-bold hover:text-orange-500"
           >
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-orange-500">
             User Profile
           </Link>
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem>
-          <Button 
+          <Button
             className="flex flex-1 font-bold bg-orange-500"
             onClick={() => logout()}
           >
@@ -34,7 +44,7 @@ const UsernameMenu = () => {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
 export default UsernameMenu;
