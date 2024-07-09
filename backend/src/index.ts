4dca16c56e8c8b7830aa8321e7851d59a3b5c7d3
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-07-09 14:39:12 +0300

    Search API request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute"
 import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -27,6 +28,8 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 
 app.use("/api/my/restaurant", myRestaurantRoute);
+
+app.use("/api/restaurant", restaurantRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the API");
