7b6f193be22c76acf892f1cfd10e11183f95e085
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-07-11 13:09:05 +0300

    Cuisines filter

---
+++ frontend/src/components/CuisineFilter.tsx
@@ -0,0 +1,92 @@
+import { cuisineList } from "@/config/restaurant-options-config";
+import React, { ChangeEvent } from "react";
+import { Label } from "./ui/label";
+import { Check, ChevronDown, ChevronUp } from "lucide-react";
+import { Button } from "./ui/button";
+
+type Props = {
+  onChange: (cuisines: string[]) => void;
+  selectedCuisines: string[];
+  isExpanded: boolean;
+  onExpandedClick: () => void;
+};
+
+const CuisineFilter = ({
+  onChange,
+  selectedCuisines,
+  isExpanded,
+  onExpandedClick,
+}: Props) => {
+  const handleCuisinesReset = () => onChange([]);
+  const handleCuisinesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const clickedCuisine = event.target.value;
+    const isChecked = event.target.checked;
+
+    const newCuisinesList = isChecked
+      ? [...selectedCuisines, clickedCuisine]
+      : selectedCuisines.filter((cuisine) => cuisine !== clickedCuisine);
+
+    onChange(newCuisinesList);
+  };
+
+  return (
+    <>
+      <div className="flex justify-between items-center px-2">
+        <div className="text-md font-semibold mb-2">Filter by cuisine</div>
+        <div
+          onClick={handleCuisinesReset}
+          className="text-sm font-semibold mb-2 underline cursor-pointer text-blue-500"
+        >
+          Reset filters
+        </div>
+      </div>
+      <div className="space-y-2 flex flex-col">
+        {cuisineList
+          .slice(0, isExpanded ? cuisineList.length : 7)
+          .map((cuisine) => {
+            const isSelected = selectedCuisines.includes(cuisine);
+            return (
+              <div className="flex">
+                <input
+                  id={`cuisine_${cuisine}`}
+                  type="checkbox"
+                  className="hidden"
+                  value={cuisine}
+                  checked={isSelected}
+                  onChange={handleCuisinesChange}
+                />
+                <Label
+                  htmlFor={`cuisine_${cuisine}`}
+                  className={`flex flex-1 items-center cursor-pointer text-sm rounded-full px-4 py-2 font-semibold ${
+                    isSelected
+                      ? "border border-green-600 text-green-600"
+                      : "border border-slate-300"
+                  }`}
+                >
+                  {isSelected && <Check size={20} strokeWidth={3} />}
+                  {cuisine}
+                </Label>
+              </div>
+            );
+          })}
+        <Button
+          variant="link"
+          className="mt-1 flex-1"
+          onClick={onExpandedClick}
+        >
+          {isExpanded ? (
+            <span className="flex flex-row items-center">
+              View less <ChevronUp />
+            </span>
+          ) : (
+            <span className="flex flex-row items-center">
+              View more <ChevronDown />
+            </span>
+          )}
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default CuisineFilter;
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import CuisineFilter from "@/components/CuisineFilter";
 import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
@@ -9,6 +10,7 @@ import { useParams } from "react-router-dom";
 export type SearchState = {
   searchQuery: string;
   page: number;
+  selectedCuisines: string[];
 };
 
 const SearchPage = () => {
@@ -16,8 +18,18 @@ const SearchPage = () => {
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
     page: 1,
+    selectedCuisines: [],
   });
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
 
   const setPage = (page: number) => {
     setSearchState((prevState) => ({
@@ -52,7 +64,16 @@ const SearchPage = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div id="cuisines-list">insert cuisines here</div>
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        />
+      </div>
       <div id="main-content" className="flex flex-col gap-5">
         <SearchBar
           onSubmit={setSearchQuery}
