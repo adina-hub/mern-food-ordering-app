499d9229633685c994c3536693f1a9f3f843f121
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-07-12 17:02:03 +0300

    Redirect to Stripe

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -11,7 +11,7 @@ import { useGetMyUser } from "@/api/MyUserApi";
 type Props = {
   onCheckout: (userFormData: UserFormData) => void;
   disabled: boolean;
-  isLoading?: boolean;
+  isLoading: boolean;
 };
 
 const CheckoutButton = ({ onCheckout, disabled, isLoading }: Props) => {
